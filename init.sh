943398e916d1d1a3cecb84b347057e357d93c67f
michakrz <kmichalski@workforcesoftware.com>
2022-01-05 16:10:21 +0100

    Add urls

--- init.sh
+++ init.sh
@@ -2,4 +2,18 @@
 # $1 - environment
 # $2 - X_VAULT_TOKEN
 echo Creating env file
-echo $1 $2
+
+BASE_APP_URL=""
+ENVIRONMENT=$1
+case $ENVIRONMENT in
+  "latest" )
+      BASE_APP_URL="https://app-latest-us8.wfs.cloud/" ;;
+  "sprint" )
+        BASE_APP_URL="https://app-sprint-us8.wfs.cloud/" ;;
+  "production" )
+        BASE_APP_URL="https://app-us8.wfs.cloud/" ;;
+
+
+esac
+echo $BASE_APP_URL
+## Create .env file with parameters
