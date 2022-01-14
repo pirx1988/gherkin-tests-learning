e30304a5102a451953f875a3fd4b0c82b239d797
michakrz <kmichalski@workforcesoftware.com>
2022-01-14 10:14:43 +0100

    asdsa

--- Jenkinsfile
+++ Jenkinsfile
@@ -9,7 +9,9 @@ pipeline {
         string(name: 'X_VAULT_TOKEN', defaultValue: '', description: 'Token for connection with Vault')
         string(name: 'SUITE_ACCOUNT', defaultValue: '', description: 'Account on which scenario/scenarios will be executed')
         string(name: 'Scenario', defaultValue: '', description: 'Scenario for execution')
-        choice(name: 'Environment', choices: ['latest', 'sprint', 'production'], description: 'Environment for tests')
+        choice(name: 'Environment', choices:
+        ['latest', 'sprint', 'production (EU1)', 'production (EU3)', 'production (US2)', 'production (US8)', 'production (AU3)'],
+        description: 'Environment for tests')
     }
     stages {
         stage('Docker container initialize') {
--- init.sh
+++ init.sh
@@ -8,18 +8,23 @@ ENVIRONMENT=$1
 X_VAULT_TOKEN=$2
 SUITE_ACCOUNT=$3
 VAULT_BASE_URL="https://vault-wf1.wfsaas.net"
-ENV_FILENAME=""
+ENV_FILENAME=".env.dev"
 
 case $ENVIRONMENT in
   "latest" )
-      BASE_APP_URL="https://app-latest-us8.wfs.cloud"
-      ENV_FILENAME=".env.latest" ;;
+      BASE_APP_URL="https://app-latest-us8.wfs.cloud" ;;
   "sprint" )
-        ENV_FILENAME=".env.sprint"
-        BASE_APP_URL="https://app-sprint-us8.wfs.cloud" ;;
-  "production" )
-        BASE_APP_URL="https://app-us8.wfs.cloud"
-        ENV_FILENAME=".env" ;;
+      BASE_APP_URL="https://app-sprint-us8.wfs.cloud" ;;
+  "production (EU1)" )
+      BASE_APP_URL="https://app-eu1.wfs.cloud" ;;
+  "production (EU3)" )
+      BASE_APP_URL="https://app-eu3.wfs.cloud" ;;
+  "production (US2)" )
+      BASE_APP_URL="https://app-us2.wfs.cloud" ;;
+  "production (US8)" )
+      BASE_APP_URL="https://app-us8.wfs.cloud" ;;
+  "production (AU3)" )
+      BASE_APP_URL="https://app-au3.wfs.cloud" ;;
 esac
 
 ## Defining the contents of the .env file:
